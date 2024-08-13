81302c26da5ddfed0096ff93239b6a4de60d2373
unknown <stud2009@yandex.ru>
2024-08-13 12:54:54 +0300

    feature: load initial db data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('1', 0, '2', '','',6000.5,null,'First Org.','',null, 'A');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('2', 0, '2', '','',1000,null,'Second Org.','',null, 'B');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('3', 0, '2', '','',534,null,'Third Org.','',null, 'C');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('4', 0, '2', '','',346,null,'Fourth Org.','',null, 'A');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('5', 0, '2', '','',1100,null,'Fifth Org.','',null, 'B');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('6', 0, '2', '','',1200,null,'Sixth Org.','',null, 'C');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('7', 0, '2', '','',1400,null,'Seventh Org.','',null, 'A');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('8', 0, '2', '','',1500,null,'Eighth Org.','',null, 'B');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('9', 0, '2', '','',1600,null,'Ninth Org.','',null, 'C');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('10', 0, '2', '','',1700,null,'Tenth Org.','',null, 'A');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('99', 0, '1', '','',1900,null,'Max','Mustermann',null, 'B');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('98', 0, '1', '','',2000,null,'John','Doe',null, 'A');
+insert into Business_Partner (ID, ETag, Type, Custom_String1, Custom_String2, Custom_Num1, Custom_Num2, Name_Line1, Name_Line2, Birth_Day, abcclass)
+values ('97', 0, '1', '','',2100,null,'Urs','Muller',null, 'C');
+
+
+insert into Business_Partner_Role (business_partnerid, Business_Partner_Role) values ('1',  'A');
+insert into Business_Partner_Role (business_partnerid, Business_Partner_Role) values ('3',  'A');
+insert into Business_Partner_Role (business_partnerid, Business_Partner_Role) values ('3',  'B');
+insert into Business_Partner_Role (business_partnerid, Business_Partner_Role) values ('3',  'C');
+insert into Business_Partner_Role (business_partnerid, Business_Partner_Role) values ('2',  'A');
+insert into Business_Partner_Role (business_partnerid, Business_Partner_Role) values ('2',  'C');
+insert into Business_Partner_Role (business_partnerid, Business_Partner_Role) values ('7',  'C');
